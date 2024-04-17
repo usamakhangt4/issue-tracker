8af97d287fd267cf35b1a292d03bc598ce822656
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-04-18 01:59:05 +0500

    Add SimpleMDE editor for issue description on new issue page
    This commit adds the SimpleMDE editor to the new issue page for users to write their issue descriptions. The necessary dependencies for SimpleMDE have also been installed.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,14 @@
+'use client'
 import {Button, TextField} from '@radix-ui/themes'
-import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
 
 const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder='Title' />
+      <SimpleMDE placeholder='Description' />
       <Button>Create New Issue</Button>
     </div>
   )
