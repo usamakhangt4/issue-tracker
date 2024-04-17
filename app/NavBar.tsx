755b193cb991325661843229eafe4733096994a5
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-04-17 23:09:13 +0500

    Add classnames package to style navigation links based on current path
    * Install classnames package
    * Import and use classnames to conditionally apply different styles to navigation links based on the current path
    * Update NavBar component to use the new

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,33 @@
+'use client';
+
+import classNames from 'classnames';
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
 
 const NavBar = () => {
+  const currentPath = usePathname()
   const links = [
     {label: 'Dashboard', href: '/'},
     {label: 'Issues', href: '/issues'}
   ]
+
+
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href={'/'}><AiFillBug /></Link>
       <ul className='flex space-x-6'>
         {
           links.map((link, index) => (
-            <Link href={link.href} key={index + link.label} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>
+            <Link href={link.href} key={index + link.label}
+              className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+
+              })}>
+              {link.label}</Link>
           ))
         }
 
