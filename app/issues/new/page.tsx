5af153028d40bd1d5cce35bdf0a6292d3a632f23
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-04-18 03:14:29 +0500

    Add issue creation functionality using react-hook-form and axios
    - Added react-hook-form for form handling
    - Added axios for API requests
    - Created a new issue with title and description using the submitted form data

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,33 @@
 import {Button, TextField} from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import {Controller, useForm} from 'react-hook-form';
+import axios from 'axios';
+import {useRouter} from 'next/navigation';
 
 
+interface IssueFormTypes {
+  title: string,
+  description: string
+
+}
 const NewIssuePage = () => {
+  const router = useRouter()
+  const {register, control, handleSubmit} = useForm<IssueFormTypes>()
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title' />
-      <SimpleMDE placeholder='Description' />
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+      await axios.post('/api/issues', data)
+      router.push('/issues')
+    })}>
+      <TextField.Root placeholder='Title' {...register('title')} />
+      <Controller name='description'
+        control={control}
+        render={({field}) =>
+          <SimpleMDE placeholder='Description' {...field} />}
+      />
+
       <Button>Create New Issue</Button>
-    </div>
+    </form>
   )
 }
 
