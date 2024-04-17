fa76e84be0792e9f0cc1bf2523e39e203457bfd2
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-04-18 03:37:02 +0500

    "Add ErrorMessage component to display form validation errors in NewIssuePage"
    This commit adds the ErrorMessage component to display form validation errors for the 'title' and 'description' fields in the NewIssuePage. The previous error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import {useState} from 'react';
 import {z} from 'zod';
 import {createIssueSchema} from '@/app/validationSchemas';
 import {zodResolver} from '@hookform/resolvers/zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 
 type IssueFormTypes = z.infer<typeof createIssueSchema>
@@ -32,13 +33,13 @@ const NewIssuePage = () => {
         }
       })}>
         <TextField.Root placeholder='Title' {...register('title')} />
-        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Controller name='description'
           control={control}
           render={({field}) =>
             <SimpleMDE placeholder='Description' {...field} />}
         />
-        {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Button>Create New Issue</Button>
       </form>
     </div>
