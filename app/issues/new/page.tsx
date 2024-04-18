6fdac5e45e2c9955bd378ca87720ee75f98bf5e4
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-04-18 22:00:09 +0500

    "Add Spinner and disable button during form submission on NewIssuePage"
    This commit adds a spinner and disables the "Create New Issue" button during form submission on the NewIssuePage. It also updates the import statement

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {Button, Callout, Text, TextField} from '@radix-ui/themes'
+import {Button, Callout, Spinner, Text, TextField} from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {Controller, useForm} from 'react-hook-form';
@@ -19,6 +19,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   })
   const [error, setError] = useState('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
 
   return (
@@ -26,9 +27,11 @@ const NewIssuePage = () => {
       {error && <Callout.Root color='red' className='mb-5'><Callout.Text>{error}</Callout.Text></Callout.Root>}
       <form className=' space-y-3' onSubmit={handleSubmit(async (data) => {
         try {
+          setIsSubmitting(true)
           await axios.post('/api/issues', data)
           router.push('/issues')
         } catch (error) {
+          setIsSubmitting(true)
           setError('An unexpected error occurred!')
         }
       })}>
@@ -40,7 +43,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder='Description' {...field} />}
         />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <Button>Create New Issue</Button>
+        <Button disabled={isSubmitting}>Create New Issue {isSubmitting && <Spinner size="1" />}</Button>
       </form>
     </div>
   )
