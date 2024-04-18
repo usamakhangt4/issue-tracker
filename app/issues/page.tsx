b1ac59b0dc34ff8f25629070f7bbe22197e1afd0
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2024-04-18 22:26:12 +0500

    "Add Table view for issues and fetch issues from Prisma"
    This commit updates the `IssuesPage` component to fetch issues from the Prisma database and display them in a table view, with columns for title, status,

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,39 @@
 // 'use client';
-import {Button} from '@radix-ui/themes'
+import prisma from '@/prisma/client';
+import {Button, Table} from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import IssueStatusBadge from '../components/IssueStatusBadge';
 
-const IssuesPage = () => {
-  console.log("err");
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
 
   return (
-    <div ><Button><Link href='/issues/new'>New Issue</Link></Button></div>
+    <div className='space-y-5'>
+      <Button>
+        <Link href='/issues/new'>New Issue</Link>
+      </Button>
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created At</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {
+            issues.map((issue) => (
+              <Table.Row key={issue.id}>
+                <Table.Cell>{issue.title} <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div></Table.Cell>
+                <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
+                <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              </Table.Row>
+            ))
+          }
+        </Table.Body>
+      </Table.Root>
+    </div>
   )
 }
 
